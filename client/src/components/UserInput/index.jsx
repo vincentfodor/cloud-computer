77370d6733ad116bc7815af5deb9792a16725497
Vincent.Fodor <Vincent.Fodor@ad.gwstut.de>
2019-07-09 10:09:34 +0200

    UI update

--- client/src/components/UserInput/index.jsx
+++ client/src/components/UserInput/index.jsx
@@ -4,7 +4,16 @@ import './index.css';
 
 const UserInput = () => {
     return (
-        <div className="userinput"></div>
+        <div className="user-input">
+            <form action="#" className="user-input-form">
+                <label className="user-input-displayname">
+                    <span className="user-input-displayname-username">root</span>
+                    @vfodor.de > 
+                </label>
+                <input type="text" name="input" className="user-input-inputfield" />
+                <button type="submit" hidden="true"></button>
+            </form>
+        </div>
     )
 }
 
