59ca9c2018693232fac2a023a083df44f5f70759
Vincent Fodor <vincent.fodor@online.de>
2019-07-10 00:18:35 +0200

    Fixed bugs, reworked code and worked on ux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
@@ -17,10 +18,50 @@ class App extends Component {
 
     switch(command[0]) {
         case 'help':
-            return `HELP     Shows this dialog`
+            return `HELP     Shows this dialog
+FS       File system`
             break;
-        case 'download':
-            
+        case 'fs':
+            if(command[1] === '--list' || command[1] === '--l') {
+              const response = await axios.get('http://localhost:3001/file-list/')
+              let output = '';
+
+              response.data.map(filename => {
+                output += filename + '\n';
+              });
+
+              return output;
+            } else if(command[1] === '--download' || command[1] === '--d') {
+              const response = await axios.get('http://localhost:3001/download/' + command[2], {
+                responseType: 'blob'
+              });
+
+              if(response.data.size === 14 && response.data.type === 'application/json') return false;
+
+              const url = window.URL.createObjectURL(new Blob([response.data]));
+              const link = document.createElement('a');
+
+              link.href = url;
+              link.setAttribute('download', command[2]);
+              document.body.appendChild(link);
+              link.click();
+              
+              return `The download for ${command[2]} is starting...`;
+            } else if(command[1] === '--help') {
+              return `File System
+
+Usage: fs [--option] [filename]
+
+OPTIONS:
+
+--help      Show this dialog
+--download
+--d         Download a file from the server
+--list
+--l         List all existing files on the server`
+            } else {
+              return false;
+            }
             break;
         case 'clear': 
             this.clearLog();
@@ -46,7 +87,7 @@ class App extends Component {
       .then((response) => {
         entry = {
           command: command,
-          response: response || 'Error',
+          response: response || 'Sorry, something went wrong. Try using --help option',
           user: 'root',
           host: '127.0.0.1'
         }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
+import fs from 'fs';
 
 import config from './config/server';
 
@@ -14,8 +15,20 @@ app.use(cors(config.corsOptions));
 app.get('/download/:file', (req, res) => {
   const file = req.params.file;
   const fileLocation = path.join(__dirname, '/files', file);
-  res.download(fileLocation, file);
+  if(fs.existsSync(fileLocation)) {
+    res.download(fileLocation, file);
+  } else {
+    res.send({
+      error: true
+    })
+  }
 });
+
+app.get('/file-list', (req, res) => {
+  const files = fs.readdirSync(path.join(__dirname, '/files'));
+
+  res.send(files);
+})
 
 app.listen(config.port, () => {
   console.log(`Server listening on port ${config.port}`);
